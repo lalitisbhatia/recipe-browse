d0d440010eb17a86fdf48eac64b069b4f75cf02a
Lalit Bhatia <lalit.bhatia@weightwatchers.com>
2023-05-25 20:33:14 -0400

    added column for similar recipes

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -13,48 +13,58 @@ const RecipeDetails = (props) => {
     const showImage =  true
     // !(recipeDetail.images===undefined || recipeDetail.images===null || Object.keys(recipeDetail.images).length === 0);
     return (
-        <div className="recipe-details">
-            {recipeDetail && <article>
-                    <h2>{recipeDetail.data.name}</h2>
-                    <div><span>  <b><i>POINTS</i> : </b></span>{Math.round(recipeDetail.data.maxPointsPrecise)} </div>
-                    <div className="row">
-                        <div className="column">
-                            <span>{recipeDetail.data.description}</span>
+        
+        
+            <div className="row">
+                <div >
+                    <div className="recipe-details">
+                        {recipeDetail && <article>
+                        <h2>{recipeDetail.data.name}</h2>
+                        <div><span>  <b><i>POINTS</i> : </b></span>{Math.round(recipeDetail.data.maxPointsPrecise)} </div>
+                        <div className="row">
+                            <div className="column">
+                                <span>{recipeDetail.data.description}</span>
+                            </div>
+                            <div className="column">
+                                <img src={ !(recipeDetail.data.images===undefined || recipeDetail.data.images===null || Object.keys(recipeDetail.data.images).length === 0)?recipeDetail.data.images.LARGE.url:""}></img>        
+                            </div>
                         </div>
-                        <div className="column">
-                            <img src={ !(recipeDetail.data.images===undefined || recipeDetail.data.images===null || Object.keys(recipeDetail.data.images).length === 0)?recipeDetail.data.images.LARGE.url:""}></img>        
-                        </div>
-                    </div>
-                    
-                    <br></br>
-                    
-                    
-                    {/* {console.log(recipeDetail.tags)} */}
-                    <p><b>Tags: </b> {recipeDetail.data.tags.reduce((acc,tag) => {
-                        return acc+=tag + ", "
-                    },"")}</p>
+                        
+                        <br></br>
+                        
+                        
+                        {/* {console.log(recipeDetail.tags)} */}
+                        <p><b>Tags: </b> {recipeDetail.data.tags.reduce((acc,tag) => {
+                            return acc+=tag + ", "
+                        },"")}</p>
 
-                    <br></br>
-                    <h3><b>Ingredients: </b></h3>
-                    <div className="recipe-ingredients-list">
-                        {recipeDetail.data.ingredients.map((ingr)=>(
-                            <div className="recipe-ingredient" key={ingr.id}>
-                                {ingr.ingredientName}
-                            </div>
-                        ))}    
+                        <br></br>
+                        <h3><b>Ingredients: </b></h3>
+                        <div className="recipe-ingredients-list">
+                            {recipeDetail.data.ingredients.map((ingr)=>(
+                                <div className="recipe-ingredient" key={ingr.id}>
+                                    {ingr.ingredientName}
+                                </div>
+                            ))}    
+                        </div>
+                        <br></br>
+                        <h3><b>Instructions: </b></h3>
+                        <div className="recipe-instruction-list">
+                            {recipeDetail.data.instructions.map((instr)=>(                            
+                                <div className="recipe-ingredient" key={instr.id}>
+                                    <b>{instr.id}</b>.  {instr.instruction}
+                                </div>
+                            ))}    
+                        </div>
+                                        
+                    </article>  }
                     </div>
-                    <br></br>
-                    <h3><b>Instructions: </b></h3>
-                    <div className="recipe-instruction-list">
-                        {recipeDetail.data.instructions.map((instr)=>(                            
-                            <div className="recipe-ingredient" key={instr.id}>
-                                <b>{instr.id}</b>.  {instr.instruction}
-                            </div>
-                        ))}    
-                    </div>
-                                    
-                </article>  }
-        </div>
+                </div>
+                <div className="similar-recipe">
+                    <p>Test column 2 for s</p>
+                </div>
+            </div>
+            
     )
 }
 export default RecipeDetails;
